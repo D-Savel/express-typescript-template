3d7bbe3a15bf026d4905c6d839edbda6f6fe11ff
David-Savel <savel@DSLAPTOP>
2024-06-07 17:23:21 +0200

    Add original code in RequestValidationError.ts in comment

--- src/errors/RequestValidationError.ts
+++ src/errors/RequestValidationError.ts
@@ -13,3 +13,21 @@ export class RequestValidationError extends CustomError {
     });
   }
 }
+
+
+// Original code with ValidationError[] type in constructor for new RequestValidationError instantiation
+
+// import { ValidationError } from 'express-validator';
+// import { CustomError } from './custom-error';
+// export class RequestValidationError extends CustomError {
+//   statusCode = 400;
+// constructor(public errors: ValidationError[]) {
+//     super();
+//     Object.setPrototypeOf(this, RequestValidationError.prototype);
+//   }
+// formatErrors() {
+//     return this.errors.map((err) => {
+//       return { message: err.msg, field: err.param };
+//     });
+//   }
+// }
